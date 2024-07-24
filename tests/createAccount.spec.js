228b1fdb2091ea04db64ea19d26e9b671a7083c3
grus4 <inter84ruslan@gmail.com>
2024-07-24 17:08:05 +0300

    Added negative tests for Email, Password< Confirm Password fields

--- tests/createAccount.spec.js
+++ tests/createAccount.spec.js
@@ -156,7 +156,7 @@ test.describe("Create an account", () => {
     await expect(registerButton).toBeDisabled();
   });
 
-  test.only("The invalid Last Name field validation", async ({ page }) => {
+  test("The invalid Last Name field validation", async ({ page }) => {
     const nameField = page.locator("#signupName");
     await nameField.fill("Max");
     const lastNameField = page.locator("#signupLastName");
@@ -170,6 +170,165 @@ test.describe("Create an account", () => {
 
     await expect(errorMessage).toHaveText("Last name is invalid");
     await expect(lastNameField).toHaveCSS("border-color", "rgb(220, 53, 69)");
+    await expect(registerButton).toBeDisabled();
+  });
+
+  test("The empty Email field validation", async ({ page }) => {
+    const nameField = page.locator("#signupName");
+    await nameField.fill("Max".trim());
+
+    const lastNameField = page.locator("#signupLastName");
+    await lastNameField.fill("Danish".trim());
+
+    const emailField = page.locator("#signupEmail");
+    await emailField.focus();
+    await emailField.blur();
+
+    const registerButton = page.locator(".modal-footer .btn-primary");
+
+    const emptyEmailFieldErrorMessage = page.locator(".invalid-feedback > p");
+    await expect(emptyEmailFieldErrorMessage).toHaveText("Email required");
+    await expect(emailField).toHaveCSS("border-color", "rgb(220, 53, 69)");
+    await expect(registerButton).toBeDisabled();
+  });
+
+  test("The invalid Email field validation", async ({ page }) => {
+    const nameField = page.locator("#signupName");
+    await nameField.fill("Max".trim());
+
+    const lastNameField = page.locator("#signupLastName");
+    await lastNameField.fill("Danish".trim());
+
+    const emailField = page.locator("#signupEmail");
+    await emailField.fill("test1:@@yopmail.com");
+
+    const passwordField = page.locator("#signupPassword");
+    await passwordField.fill("Qwert_1234");
+
+    const registerButton = page.locator(".modal-footer .btn-primary");
+
+    const invalidEmailErrorMessage = page.locator(".invalid-feedback > p");
+    await expect(invalidEmailErrorMessage).toHaveText("Email is incorrect");
+    await expect(emailField).toHaveCSS("border-color", "rgb(220, 53, 69)");
+    await expect(registerButton).toBeDisabled();
+  });
+
+  test("The empty Password field validation", async ({ page }) => {
+    const nameField = page.locator("#signupName");
+    await nameField.fill("Max".trim());
+
+    const lastNameField = page.locator("#signupLastName");
+    await lastNameField.fill("Danish".trim());
+
+    const emailField = page.locator("#signupEmail");
+    await emailField.fill(generateRandomEmail());
+
+    const passwordField = page.locator("#signupPassword");
+    await passwordField.focus();
+    await passwordField.blur();
+
+    const registerButton = page.locator(".modal-footer .btn-primary");
+
+    const emptyPasswordFieldErrorMessage = page.locator(
+      ".invalid-feedback > p"
+    );
+    await expect(emptyPasswordFieldErrorMessage).toHaveText(
+      "Password required"
+    );
+    await expect(passwordField).toHaveCSS("border-color", "rgb(220, 53, 69)");
+    await expect(registerButton).toBeDisabled();
+  });
+
+  test("The invalid Password field validation", async ({ page }) => {
+    const nameField = page.locator("#signupName");
+    await nameField.fill("Max".trim());
+
+    const lastNameField = page.locator("#signupLastName");
+    await lastNameField.fill("Danish".trim());
+
+    const emailField = page.locator("#signupEmail");
+    await emailField.fill(generateRandomEmail());
+
+    const passwordField = page.locator("#signupPassword");
+    await passwordField.fill("123rtewi");
+
+    const confirmPasswordField = page.locator("#signupRepeatPassword");
+    await confirmPasswordField.focus();
+
+    const registerButton = page.locator(".modal-footer .btn-primary");
+
+    const invalidPasswordErrorMessage = page.locator(".invalid-feedback > p");
+    await expect(invalidPasswordErrorMessage).toHaveText(
+      "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
+    );
+    await expect(passwordField).toHaveCSS("border-color", "rgb(220, 53, 69)");
+    await expect(registerButton).toBeDisabled();
+  });
+
+  test("The empty Confirm Password field validation", async ({ page }) => {
+    const nameField = page.locator("#signupName");
+    await nameField.fill("Max".trim());
+
+    const lastNameField = page.locator("#signupLastName");
+    await lastNameField.fill("Danish".trim());
+
+    const emailField = page.locator("#signupEmail");
+    await emailField.fill(generateRandomEmail());
+
+    const passwordField = page.locator("#signupPassword");
+    await passwordField.fill("Qwert_1234");
+
+    const confirmPasswordField = page.locator("#signupRepeatPassword");
+    await confirmPasswordField.focus();
+    await confirmPasswordField.blur();
+
+    const registerButton = page.locator(".modal-footer .btn-primary");
+
+    const emptyConfirmPasswordFieldErrorMessage = page.locator(
+      ".invalid-feedback > p"
+    );
+    await expect(emptyConfirmPasswordFieldErrorMessage).toHaveText(
+      "Re-enter password required"
+    );
+    await expect(confirmPasswordField).toHaveCSS(
+      "border-color",
+      "rgb(220, 53, 69)"
+    );
+    await expect(registerButton).toBeDisabled();
+  });
+
+  test("Verifying mismatching a Password in the Password and Confirm Password fields", async ({
+    page,
+  }) => {
+    const nameField = page.locator("#signupName");
+    await nameField.fill("Max".trim());
+
+    const lastNameField = page.locator("#signupLastName");
+    await lastNameField.fill("Danish".trim());
+
+    const emailField = page.locator("#signupEmail");
+    await emailField.fill(generateRandomEmail());
+
+    const passwordField = page.locator("#signupPassword");
+    await passwordField.fill("Qwert_1234");
+
+    const confirmPasswordField = page.locator("#signupRepeatPassword");
+    await confirmPasswordField.fill("Qwert_12345");
+    await confirmPasswordField.focus();
+    await confirmPasswordField.blur();
+
+    const registerButton = page.locator(".modal-footer .btn-primary");
+
+    const passwordMismatchingErrorMessage = page.locator(
+      ".invalid-feedback > p"
+    );
+    await expect(passwordMismatchingErrorMessage).toHaveText(
+      "Passwords do not match"
+    );
+    await expect(confirmPasswordField).toHaveCSS(
+      "border-color",
+      "rgb(220, 53, 69)"
+    );
     await expect(registerButton).toBeDisabled();
   });
 });
